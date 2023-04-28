9f322cea5bec32f04af82bcdb2588725722ebe04
Cristian Le <cristian.le@mpsd.mpg.de>
2023-04-28 22:26:40 +0200

    Fix ReadTheDocs build
    Signed-off-by: Cristian Le <git@lecris.dev>

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import sys
 
 project = 'ThingFramework'
 copyright = '2023, Cristian Le'
@@ -26,7 +28,6 @@ source_suffix = [".md"]
 pygments_style = "sphinx"
 html_css_files = ["breathe.css"]
 
-
 html_theme = 'furo'
 html_static_path = ['_static']
 
@@ -38,3 +39,22 @@ myst_enable_extensions = [
 cmake_build_dir = os.getenv("DOXYGEN_DIR", "../cmake-build-docs/doc/xml")
 breathe_projects = {"ThingFramework": cmake_build_dir}
 breathe_default_project = "ThingFramework"
+
+
+def generate_doxygen_xml(app):
+    """Run the doxygen make commands"""
+
+    read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+    if read_the_docs_build:
+        os.makedirs("../cmake-build-docs/doc", exist_ok=True)
+        try:
+            retcode = subprocess.call("doxygen", shell=True)
+            if retcode < 0:
+                sys.stderr.write(f"doxygen terminated by signal {-retcode}")
+        except OSError as e:
+            sys.stderr.write(f"doxygen execution failed: {e}")
+
+
+def setup(app):
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", generate_doxygen_xml)
